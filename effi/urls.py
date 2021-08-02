28cd949e27d738f6ce59e1efbc057622de6c5e90
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-08-02 10:30:34 +0900

    ver3_3

--- effi/urls.py
+++ effi/urls.py
@@ -5,5 +5,4 @@ from django.views.generic import TemplateView
 app_name = 'effi'
 urlpatterns = [
     path('upload/', views.upload, name='upload'),
-    path('showall/', views.showall, name='showall'),
 ]
