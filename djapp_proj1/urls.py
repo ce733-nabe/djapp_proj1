502ed07dcb6cee85ac50ff76e2c43e2022de5c26
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-08-05 13:07:13 +0900

    ver3_27

--- djapp_proj1/urls.py
+++ djapp_proj1/urls.py
@@ -26,11 +26,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('effi/', include('effi.urls')),
     path('album/', include('album.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ] 
+    ]
+    
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
